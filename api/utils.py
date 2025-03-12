62d2dcae1bb2fcc37d93cd6971f9351545b04103
shulaev ivan <mail@ntdservice.ru>
2025-03-12 22:39:57 +0300

    add some fixes

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,10 @@
 import os
 import json
-
+import re
+from datetime import datetime
+from pprint import pprint
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 def create_upload_folders():
     upload_files = f'{os.getcwd()}/upload_files/'
@@ -51,13 +55,101 @@ def rebuild_json():
         with open(f'{os.getcwd()}/download/fixtures_city2.json', 'w') as file:
             json.dump(result_data, file, ensure_ascii=False, indent=4)
 
+
+def get_time(date):
+    result_str = ''
+
+    def add_zero_to_time(value):
+        if value < 10:
+            return f'0{value}'
+        return value
+    
+    time_arr = [
+        {"name": "hour", "value": date.hour},
+        {"name": "minute", "value": date.minute},
+        {"name": "second", "value": date.second},
+        {"name": "day", "value": date.day},
+        {"name": "month", "value": date.month},
+        {"name": "year", "value": date.year},
+    ]
+    
+    for time_obj in time_arr:
+        time_obj['value'] = add_zero_to_time(time_obj['value'])
+        if time_obj['name'] == 'hour' or time_obj['name'] == 'minute':
+            time_obj['value'] = f"{time_obj['value']}:"
+        elif time_obj['name'] == 'second':
+            time_obj['value'] = f"{time_obj['value']} Дата: "
+        elif time_obj['name'] == 'day' or time_obj['name'] == 'month':
+            time_obj['value'] = f"{time_obj['value']}/"
+        
+        result_str = result_str + str(time_obj['value'])
+
+    return f"Время: {result_str}"
+
+
 def select_email_template_by_order(order_type):
-    json_path = f'{os.getcwd()}/email_templates/email_tepmlates.json'
+    json_path = f'{os.getcwd()}/email_templates/email_templates.json'
+
     with open(json_path, 'r') as json_file:
         template_data = json.load(json_file)
     
     target_template = list(filter(lambda item: item.get('template_name') == order_type, template_data))
     if len(target_template) > 0:
         return target_template[0]
+    
     return target_template
+
+
+def send_order_to_main_email(email_template, email_data, time='', order_number=''):
+    try:
+        msg_mail = EmailMessage(
+            f"{email_template.get('email_subject')}", 
+            f"""
+                <h4>Описание:</h4>
+                <p>{email_template.get('email_body_description')}</p>
+
+                <h4>Данные:</h4>
+                <ul>
+                    {''.join(f"<li>{item.get('value')} : {email_data.get(item.get('name'))}</li>" for item in email_template.get('fields'))}
+                </ul>
+
+                <p>№ запроса: <strong>{order_number}</strong></p>
+                <p><strong>{time}</strong></p>
+
+            """,
+            f'{settings.EMAIL_HOST_USER}', [f"{settings.EMAIL_ORDER_ADDRESS}"]
+        )
+        msg_mail.content_subtype = "html"
+        msg_mail.send()
+    except Exception as err:
+        write_email_err_log(err, email_template.get('template_name'))
+
+
+def write_email_err_log(err, template, order_num=''):
+    current_date = datetime.now().replace(microsecond=0)
+    err_description = f'order_num: {order_num} date: {current_date} send_form: {template}  err_descr: {err}'
+    log_file_path = f'{os.getcwd()}/logs/mail_err.txt'
+    
+    with open(log_file_path, 'a+') as file:
+        file.write(f'{err_description} \n')
+
+def write_access_view_err_log(err, view_name=''):
+    current_date = datetime.now().replace(microsecond=0)
+    err_description = f'view_func: {view_name} date: {current_date} err_descr: {err}'
+    log_file_path = f'{os.getcwd()}/logs/access_views_err.txt'
+
+    with open(log_file_path, 'a+') as file:
+        file.write(f'{err_description} \n')
+    
+    
+
+def send_email_to_client(email_data, client_email):
+    pass
+
+def validate_email(email_str):
+    email_pattern = r'\S*\@\S*\.\w{2,10}$'
+    check_email_valid = re.match(email_pattern, email_str)
+    if check_email_valid and len(check_email_valid.group(0)) > 0:
+        return True
+    return False
 
--- api/views.py
+++ api/views.py
@@ -21,6 +21,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 from docxtpl import DocxTemplate
+from .utils import write_access_view_err_log, validate_email
+from.utils import get_time, select_email_template_by_order, send_order_to_main_email
 
 from .models import CallbackRequests, Client, Order, ClientOrder, ConsultRequest, ClientOrderFile, \
     CoperationRequest, CoperationRequestFile, CityData, QuizOrder, QuizQuestionOrder, QuizTzOrder, \
@@ -53,37 +55,56 @@ def default(request):
     
 class CallbackRequestView(APIView):
     permission_classes = [IsAuthenticated, ]
+    order_type = 'callback_req'
 
     def get(self, request):
+        try:
+            pass
+        except Exception as err:
+            write_access_view_err_log(err, 'CallbackRequestView')
         
         return Response({'message': 'ok'}, status=status.HTTP_200_OK)
     
     def post(self, request):
-        req_body = request.data
-        not_format_date = datetime.datetime.now()
-        time = get_time(not_format_date)
-        email_data = {
-            'phone': req_body.get('phone'),
-            'time': req_body.get('time'),
-            'type': req_body.get('type'),
-        }
+        try:
+            req_body = json.loads(request.body)
 
-        if email_data.get('phone'):
-            CallbackRequests.objects.create(
-                phone = email_data.get('phone'),
-                name = '',
-                request_time = not_format_date
-            ).save()
+            not_format_date = datetime.datetime.now()
+            time = get_time(not_format_date)
+            email_data = {
+                'phone': req_body.get('phone'),
+                'time': req_body.get('time'),
+                'type': req_body.get('type'),
+            }
 
-            send_mail(
-                'Заказ обратного звонка', 
-                f"Пришел запрос перезвонить клиенту по поводу консультации (контрактное производство)\n, Телефон клиента: {email_data['phone']}\n {time}", 
-                'django_mail@cosmtech.ru', ["pro@cosmtech.ru"]
+            if email_data.get('phone'):
+                email_template = select_email_template_by_order(self.order_type)
+                send_email = send_order_to_main_email(email_template, email_data, time)
+                response_description = email_template.get('response_description')
+
+                CallbackRequests.objects.create(
+                    phone = email_data.get('phone'),
+                    name = '',
+                    request_time = not_format_date
+                ).save()
+
+                return Response(
+                    {'message': 'ok', 'description': response_description}, 
+                    status=status.HTTP_201_CREATED
+                )
+            return Response(
+                {'message': 'err phone not valid'}, 
+                status=status.HTTP_200_OK
             )
 
-            return Response({
-                'message': 'ok', 'description': 'Спасибо! Вам перезвонят в течении 30 мин'}, status=status.HTTP_201_CREATED)
-        return Response({'message': 'err phone not valid'}, status=status.HTTP_200_OK)
+        except Exception as err:
+            write_access_view_err_log(err, 'CallbackRequestView')
+            err_description = 'Очень жаль, но что-то пошло не так, отправьте запрос вручную на pro@cosmtech.ru'
+            
+            return Response(
+                {'message': 'ok', 'description': err_description}, 
+                status=status.HTTP_201_CREATED
+            )
     
 class RequestConsultView(APIView):
 
@@ -136,74 +157,91 @@ class RequestConsultView(APIView):
         send_description = f"Спасибо за обращение {client_data.get('client_name')}, с вами свяжутся в ближайшее время (30 - 60 мин)!"
         
         
-        return Response({'status': 'ok', 'description': send_description}, status=status.HTTP_200_OK)
+        return Response(
+            {'status': 'ok', 'description': send_description}, 
+            status=status.HTTP_200_OK
+        )
 
 class RequestOrderView(APIView):
     permission_classes = [IsAuthenticated, ]
+    order_type = 'order_req'
     
     def post(self, request):
-
-        req_body = request.data
-        order_data = {
-            "name": req_body.get('name'),
-            "email": req_body.get('email'),
-            "phone": req_body.get('phone'),
-            "comment": req_body.get('comment'),
-            "options": req_body.get('options')
-        }
+        try:
+            req_body = json.loads(json.dumps(request.data))
+            email_template = select_email_template_by_order(self.order_type)
+            response_description = email_template.get('response_description')
+            order_data = {
+                "name": req_body.get('name'),
+                "email": req_body.get('email'),
+                "phone": req_body.get('phone'),
+                "comment": req_body.get('comment'),
+                "options": req_body.get('options')
+            }
         
-        if not order_data['email'] and not order_data['phone']:
-            return Response({'status': 'not found', 'description': 'email or phone not valid'}, status=status.HTTP_200_OK)
+            if not order_data.get('email') and not order_data.get('phone'):
+                return Response({'status': 'not found', 'description': 'email or phone not valid'}, status=status.HTTP_200_OK)
         
-        if order_data['email'] or order_data['phone']:
-            if order_data['phone'] and order_data['email']:
-                target_client = Client.objects.filter(email=order_data['email'], phone=order_data['phone'])
-            elif order_data['email'] and not order_data['phone']:
-                target_client = Client.objects.filter(email=order_data['email'])
-            elif order_data['phone'] and not order_data['email']:
-                target_client = Client.objects.filter(phone=order_data['phone'])
+            if order_data.get('email') or order_data.get('phone'):
+                if order_data.get('phone') and order_data.get('email'):
+                    target_client = Client.objects.filter(email=order_data['email'], phone=order_data['phone'])
+                elif order_data.get('email') and not order_data.get('phone'):
+                    target_client = Client.objects.filter(email=order_data['email'])
+                elif order_data.get('phone') and not order_data.get('email'):
+                    target_client = Client.objects.filter(phone=order_data['phone'])
                 
                
-            if not target_client.exists():
-                Client.objects.create(
-                    name=order_data['name'], 
-                    phone=order_data['phone'], 
-                    email=order_data['email'],
+                if not target_client.exists():
+                    Client.objects.create(
+                        name=order_data.get('name'), 
+                        phone=order_data.get('phone'), 
+                        email=order_data.get('email'),
+                    ).save()
+                    target_client = Client.objects.filter(name=order_data.get('name'), phone=order_data.get('phone'), email=order_data.get('email'))
+            
+                if not order_data.get('options'):
+                    order_data['options'] = 'Расчет контрактного производства'
+
+                order = generate_order_number(order_data.get('options'), target_client[0].id)
+                target_order, created = Order.objects.get_or_create(order_type=order['type'], order_date=order['date'])
+                target_order.save()
+
+                ClientOrder.objects.create(
+                    client_id=target_client.first(), 
+                    order_id=Order.objects.filter(id=target_order.id).first(),
+                    order_number = order['number'], 
+                    order_date = order['date'],
+                    oreder_description = order_data['comment'],
+                    order_option = order_data['options']
                 ).save()
-                target_client = Client.objects.filter(name=order_data['name'], phone=order_data['phone'], email=order_data['email'])
+
+                not_format_date = datetime.datetime.now()
+                time = get_time(not_format_date)
+                client_data = {
+                    'client_name': order_data['name'],
+                    'client_phone': order_data['phone'],
+                    'client_email': order_data['email'],
+                    'order_number': order['number'],
+                    'call_option': 'Любой',
+                    'order_type_name': order_data['options'],
+                    'client_comment': order_data['comment'],
+                }
+                send_email = send_order_to_main_email(email_template, client_data, time)
+
+                if validate_email(client_data.get('client_email')):
+                    print('tset ok')
+                    # send_mail_to_client(client_data)
+
+                return Response(
+                    {'message': 'ok', 'description': f"{response_description} {client_data.get('order_number')}"}, 
+                    status=status.HTTP_201_CREATED
+                )
             
-            if not order_data['options']:
-                order_data['options'] = 'none'
-            order = generate_order_number(order_data['options'], target_client[0].id)
-            target_order, created = Order.objects.get_or_create(order_type=order['type'], order_date=order['date'])
-            target_order.save()
-
-            ClientOrder.objects.create(
-                client_id=target_client.first(), 
-                order_id=Order.objects.filter(id=target_order.id).first(),
-                order_number = order['number'], 
-                order_date = order['date'],
-                oreder_description = order_data['comment'],
-                order_option = order_data['options']
-            ).save()
+        except Exception as err:
+            write_access_view_err_log(err, 'RequestOrderView')
+            err_description = 'Очень жаль, но что-то пошло не так, отправьте запрос вручную на pro@cosmtech.ru'
             
-            client_data = {
-                'client_name': order_data['name'],
-                'client_phone': order_data['phone'],
-                'client_email': order_data['email'],
-                'order_number': order['number'],
-                'call_option': '',
-                'order_type_name': order_data['options'],
-                'client_comment': order_data['comment'],
-            }
-
-            send_order_mail(client_data)
-            send_mail_to_client(client_data)
-
-            return Response({
-                'message': 'ok', 'description': f"Спасибо! Запрос отправлен, Номер запроса: №{order['number']}"}, status=status.HTTP_201_CREATED)
-
-        return Response({'status': 'ok'}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'ok', 'description': err_description}, status=status.HTTP_201_CREATED)
     
 class ContactsRequestView(APIView):
     permission_classes = [IsAuthenticated, ]
@@ -1322,37 +1360,6 @@ def get_request_name(value):
 
     }
     return request_types.get(value)
-
-
-def get_time(date):
-    result_str = ''
-
-    def add_zero_to_time(value):
-        if value < 10:
-            return f'0{value}'
-        return value
-    
-    time_arr = [
-        {"name": "hour", "value": date.hour},
-        {"name": "minute", "value": date.minute},
-        {"name": "second", "value": date.second},
-        {"name": "day", "value": date.day},
-        {"name": "month", "value": date.month},
-        {"name": "year", "value": date.year},
-    ]
-    
-    for time_obj in time_arr:
-        time_obj['value'] = add_zero_to_time(time_obj['value'])
-        if time_obj['name'] == 'hour' or time_obj['name'] == 'minute':
-            time_obj['value'] = f"{time_obj['value']}:"
-        elif time_obj['name'] == 'second':
-            time_obj['value'] = f"{time_obj['value']} Дата: "
-        elif time_obj['name'] == 'day' or time_obj['name'] == 'month':
-            time_obj['value'] = f"{time_obj['value']}/"
-        
-        result_str = result_str + str(time_obj['value'])
-
-    return f"Время: {result_str}"
 
 def keys_form_camel_case_to_python_style(data):
     result_dict = dict()
